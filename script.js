37fc429fad4472c38d5d5dd6539c7e9947eef6e3
Ayushman Behera <ayushmandav2805@gmail.com>
2025-09-18 20:53:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,109 @@
+
+const pandals = {
+  asansol: [
+    
+    {
+      name: "Kalyanpur Scheme-2",
+      desc: "Famous for its classic idol decoration",
+      img: "https://tse1.mm.bing.net/th/id/OIP.dm2qoY3e3ZQ81xkdXX3ZJwHaE8?pid=Api&P=0&h=180",
+      link: "https://maps.app.goo.gl/ptsCVokFZb76UHdp6"  
+    },
+    {
+      name: "Kalyanpur Adi Puja",
+      desc: "Traditional pandal with vibrant lights",
+      img: "https://tse1.mm.bing.net/th/id/OIP.TbT4reTJ2KE9M4fTWjiBRwHaE8?pid=Api&P=0&h=180",
+      link: "https://maps.app.goo.gl/ccS88JSQeG2L5vCa9"  
+    },
+    {
+      name: "K-sector Kalyanpur Housing",
+      desc: "Creative lighting with a large loving crowd.",
+      img: "https://tse3.mm.bing.net/th/id/OIP._nEPfniYF8m0jKj70ud1WAHaEK?pid=Api&P=0&h=180",
+      link: "https://maps.app.goo.gl/67ibi8rDM23XLVva6"
+    },
+    {
+      name: "Rabindranagar",
+      desc: "Popular for colorful decorations",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr8dTAf0zdEToWBHOcypyOIQQeXDJrRfRmaXEHxsOh4-rSoqc320xV-y8kmLHaCsRaWfVK5FxqRJSlao6LlHiOcDpZyTh3wJUBeDC_iVrGdcuYvuZ5c8rVrsy6373tpxcYNEbIE=w408-h306-k-no",
+      link: "https://maps.app.goo.gl/77swGu3R4D9aAmiz5"  
+    },
+     {
+      name: "Radhanagar Road Atheletic Club",
+      desc: "Large pandal with food stalls",
+      img: "https://tse2.mm.bing.net/th/id/OIP.-n9D-bj3cIAy7pNe1Xgj7QHaEK?pid=Api&P=0&h=180",
+      link: "https://maps.app.goo.gl/THNcDcBQ3xPfsEir8"  
+    },
+     {
+      name: "Court Road Puja Comittee",
+      desc: "Small Pandal but concept Driven",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4noFqHo3VS1jqyPX73sY8FQX5XwCJp_J0UomsEqLIvgD75HHCEkVDZPA8OeJvmfy2yYWquAxXVhLRBMG2GXGziv7g9Pz5V4-6d1Us4BL80Ec3QdiirPdBQKuZRaNipP2cA6WpGYq=w533-h240-k-no",
+      link: "https://maps.app.goo.gl/3TuebFS2v1qj7RJg7"  
+    },
+    {
+      name: "Netaji Sporting Club, Burnpur",
+      desc: "Grand idol and vibrant cultural events..",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4npKGwr2zx8AjQRy8ezyTBDE4MFkHedu0K8Jg5BbcZKzlqlY2dp8QWqEvBhil3bSdbYtdvLOzlB8YKtTetOf7I3RFrtg4fJMcQPG5Kf474eUTE3YVyvYJzewjT7PWDsiNo0kxhAC=w408-h307-k-no",
+      link: "https://maps.app.goo.gl/pHZw1W2FSgShU8Db7"
+    },
+    {
+      name: "Gandhinagar / Panchgachia",
+      desc: "Traditional Varanasi and ganga Arati theme",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrOcPNFaurVYGHZWIOdfq2H0gcYccJA5DoBw-MWoaXZtqy8Ozg0P1VUeH33ERnBzKM1BLRIh-gy9IKvliQECZ_VUawcjq1JwepmeyIWtavX_x_1rRMQnvVmhW5eeHgD7L1QRwBpiQ=w408-h306-k-no",
+      link: "https://maps.app.goo.gl/tqkBJyUcuaZfyrKV7"  
+    },
+    {
+      name: "Dheomain Colliery",
+      desc: "Famous for its lighting.",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nokoTYPg4dBo2fdw5cljPMv_yeHT1UIbtL1-g2OQ16kXK6ZQCFf3rOv7cd2EqckifvlaZWQfE1PGfsQ6-1oiXYOBN3lBq3-OpiDMYZQ9TjuAtuUoiQ6ZVTk4K0pQA90h3fC8RaNFw=w408-h306-k-no",
+      link: "https://maps.app.goo.gl/FPatu2LpnyvYTmMP8" 
+    },
+     {
+      name: "Gopalpur",
+      desc: "Small but concept packed pendel.",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nojndFwuBiEjgoliTH44MMbOEk3dFDPkDv1vOqjSx5pVrEirzceFLftT_z0AOK2cLqsYfLkeED3EuG4FPoNTc4Ri3WV3X97R_3HYKbQRNQDw4WHof1wxs1KN2Qfem5v_uJWGyvx=w408-h306-k-no",
+      link: "https://maps.app.goo.gl/SyF91VnGxvGSQCZo6" 
+    },
+     {
+      name: "Apcar Garden",
+      desc: "Famous for it after-puja shows.",
+      img: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqyGhyRyCCT3IYYUxshyx2cmabuPrCMlAY1y4iCnnjWlRfG3U9iJsW3K_jx2L4iUWu2HRp6fie8pcmvmNv0iGbIFcIQ1tLMzfjU5a-Ww5BZBjnMYuLAD4kB2i3f-lGXFOGpOaJqxQ=w408-h305-k-no",
+      link: "https://maps.app.goo.gl/2jXTA2cRNYPLiyEk7" 
+    },
+
+
+   
+  ],
+  durgapur: [
+    {
+      name: "Gopalmath",
+      desc: "Modern decorations; light installations.",
+      img: "https://tse4.mm.bing.net/th/id/OIP.jkuPgrvz-icALTlamXwpQQHaEK?pid=Api",
+      link: "#"  
+    },
+    {
+      name: "Fuljhore",
+      desc: "Food stalls, community events.",
+      img: "https://tse1.mm.bing.net/th/id/OIP.PCGMBaLdby15HfPB4qogegHaKb?pid=Api",
+      link: "#"  
+    }
+   
+  ]
+};
+
+function render(city) {
+  const container = document.getElementById("pandal-list");
+  container.innerHTML = "";
+  const list = pandals[city] || [];
+  list.forEach(p => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <img src="${p.img}" alt="${p.name}">
+      <div class="card-content">
+        <h3>${p.name}</h3>
+        <p>${p.desc}</p>
+        <a href="${p.link}" target="_blank">📍 Open Map</a>
+      </div>
+    `;
+    container.appendChild(card);
+  });
+}
